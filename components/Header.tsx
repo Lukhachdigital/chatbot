f8e954676a523d867c1b72eef8ebfd5bcd791960
Lukhachdigital <huynhxuyensondirector@gmail.com>
2025-10-16 13:23:13 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ const Header: React.FC = () => {
       </div>
 
       <div className="flex items-center space-x-4 mx-6 flex-shrink-0">
-        <a href="https://lamyoutubeai.com/support" target="_blank" rel="noopener noreferrer">
+        <a href="https://zalo.me/g/fzzokk254" target="_blank" rel="noopener noreferrer">
           <button className={`${baseButtonClasses} ${supportButtonClasses}`}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0110 5c1.657 0 3 1.343 3 3 0 1.05-.6 1.954-1.448 2.457A1 1 0 1110.5 12.5v.5a1 1 0 11-2 0v-.5c0-.495.235-.954.632-1.293C9.67 10.375 10 9.875 10 9c0-.551-.449-1-1-1a1 1 0 00-1 1zM9 14a1 1 0 112 0 1 1 0 01-2 0z" clipRule="evenodd" />
